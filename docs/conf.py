e978f08cd2367b0bbaf0a1693af7d71b5d70196b
Wei Chen <wchen@anl.gov>
2026-01-05 17:38:55 -0600

    docs: switch Sphinx theme to furo

--- docs/conf.py
+++ docs/conf.py
@@ -260,21 +260,7 @@ myst_enable_extensions = [
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
-
-# Theme options are theme-specific and customize the look and feel of a
-# theme further.  For a list of options available for each theme, see the
-# documentation.
-html_theme_options = {
-    "logo_only": False,
-    "prev_next_buttons_location": "bottom",
-    "style_external_links": False,
-    "collapse_navigation": False,
-    "sticky_navigation": True,
-    "navigation_depth": 4,
-    "includehidden": True,
-    "titles_only": False,
-}
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
